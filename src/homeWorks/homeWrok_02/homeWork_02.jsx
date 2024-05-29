57446cc2dff5ed825071c234d3d7ea705b0d61fe
nozhik09 <nozhik09@gmail.com>
2024-05-29 14:19:59 +0200

    homeWork_02

---
+++ src/homeWorks/homeWrok_02/homeWork_02.jsx
@@ -0,0 +1,13 @@
+import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import "./style.css";
+
+function HomeWork2() {
+  return (
+    <div className="profileCard_container">
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  );
+}
+export default HomeWork2;
